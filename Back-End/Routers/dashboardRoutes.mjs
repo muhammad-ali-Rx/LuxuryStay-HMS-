9376fc747a3344d25890ad2ea74015928cbd1b77
muhammad-ali-Rx <alimuhammadali8753@gmail.com>
2026-01-21 06:18:35 +0500

    change him

--- Back-End/Routers/dashboardRoutes.mjs
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getDashboardStats } from "../Controller/dashboardController.mjs";
-
-const router = express.Router();
-
-router.get("/stats", getDashboardStats);
-
-export default router;
